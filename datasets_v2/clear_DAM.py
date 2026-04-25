b50e62f242197b28b0fd3ee69ff5a4c09d5ba21c
Denys Lunhul <lundenis15@gmail.com>
2026-04-25 16:20:35 +0300

    cleared and concatenated all datasets into one

--- datasets_v2/clear_DAM.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("../datasets_v1/DAM.csv")
-df.drop(columns=["Decl_Sale", "Decl_Buy"], inplace=True)
-df.to_csv("DAM.csv", index=False)
--- datasets_v2/clear_IDM.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("../datasets_v1/IDM.csv")
-df.drop(columns=["Decl_Sale", "Decl_Buy", "Min_Price", "Max_Price"], inplace=True)
-df.to_csv("IDM.csv")
