7096514cb309db9f9f6826ef3577bcd3228a17f5
Denys Lunhul <lundenis15@gmail.com>
2026-04-29 21:52:45 +0300

    weather fetcher recieves custom azimuth and tilt

--- data_providers/orchestrator/data_combiner.py
+++ data_providers/orchestrator/data_combiner.py
@@ -7,7 +7,27 @@ from data_providers.market_manager.IDM_DAM_features import fetch_DAM
 from data_providers.weather.weather import fetch_weather
 from data_providers.time.time_features import fetch_time
 
-def combine():
+
+from backend.core.database import SessionLocal
+from backend.models.site import SystemConfig
+
+
+def get_solar_parameters():
+    db = SessionLocal()
+    config_record = db.query(SystemConfig).order_by(SystemConfig.id.desc()).first()
+    db.close()
+
+    if config_record and config_record.settings:
+        solar = config_record.settings.get("solar", {})
+        return solar.get("tilt", 35), solar.get("azimuth", 0)
+
+    return 35, 0
+
+
+def combine(tilt = None, azimuth = None):
+    if tilt is None or azimuth is None:
+        tilt, azimuth = get_solar_parameters()
+
     today = datetime.today()
     time = fetch_time(today)
     grid = fetch_grid(today)
@@ -17,5 +37,8 @@ def combine():
     dataset = pd.concat([time, grid, load, weather, DAM], axis=1)
     return dataset
 
-df = combine()
-df.to_csv("combined.csv", index=False)
+
+if __name__ == "__main__":
+    t, a = get_solar_parameters()
+    df = combine(t, a)
+    df.to_csv("combined.csv", index=False)
